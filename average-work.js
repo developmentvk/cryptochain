38aeb65d066e70664dce8281a951457c83622497
developmentvk <development.vk@hotmail.com>
2020-01-14 23:57:40 +0530

    Optimized Code

--- average-work.js
+++ average-work.js
@@ -3,6 +3,9 @@ const Blockchain = require('./blockchain');
 const blockchain = new Blockchain();
 
 blockchain.addBlock({ data: 'initial' });
+
+console.log('first block', blockchain.chain[blockchain.chain.length - 1]);
+
 let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = [];
@@ -19,6 +22,6 @@ for (let i = 0; i < 10000; i++) {
 
     average = times.reduce((total, num) => (total + num)) / times.length;
 
-    console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+    // console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
 
 }
